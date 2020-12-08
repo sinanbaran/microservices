ff540221afb80a8505144a45c033d1fa60fc9041
SINAN BARAN <SINAN.BARAN@lcwaikiki.com>
2020-12-09 00:55:32 +0300

    add gateway and order command project

---
+++ src/OrderCommand/Order.Command.API/Order.Command.API/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Order.Command.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("");
+        }
+
+    }
+}
